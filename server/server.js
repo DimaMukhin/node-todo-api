10c9f8c3d00aaa6af3ff738fbb8d20eee914bdd4
DimaMukhin <dima9348@gmail.com>
2017-08-05 17:27:23 -0500

    Added GET by id support to todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoos} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -24,6 +25,22 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
   }, (e) => {
     res.status(400).send(e);
   });
